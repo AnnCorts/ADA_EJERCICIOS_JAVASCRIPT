6bca7e7ae78492fd77b93cea75cb7243721aa2d5
AntonellaCorts <apcorts@gmail.com>
2026-01-15 15:50:01 -0300

    clase12

--- CLASE12/c12-ej9.js
+++ CLASE12/c12-ej9.js
@@ -10,13 +10,13 @@ Los gastos estarán en una matriz de 4x7, cada fila representa una semana y
 cada columna un día. Es decir, fila 0, semana 1, fila 1, semana 2, etc. Columna 0,
 día 1, columna 1, día 2, etcétera.
 
-a) Lo que nos solicitan es dar el total de gastos en una semana. Recordemos
+a) Lo que nos solicitan es dar el total de gastos en una SEMANA. Recordemos
 que cada fila representa una semana, es decir, si nos indican semana 2
 tenemos que sumar la fila 1 de la matriz. Recordar que las matrices
 comienzan siempre en posición 0.
 
 b) La aplicación también tendrá una parte de estadísticas, para esto nos
-solicitan dar el total de un día en particular, por ejemplo del día 3, acá
+solicitan dar el total de un DIA en particular, por ejemplo del día 3, acá
 también tengamos en cuenta lo que ocurre con las filas, ya que las columnas
 también comienzan en 0.
 
@@ -36,6 +36,7 @@ Semana 4   800  1250  1430  2100  1980  1270  950
 */
 
 const prompt = require (`prompt-sync`)();  
+
 
 //matriz
 let matriz = [
@@ -65,31 +66,157 @@ let idSemana =nroSemana  - 1; //ahora traducimos semana nro a elemento de la mat
 
 console.log(`el gasto de la semana numero ${nroSemana} es: ${suma_semana}`);
 
-/*b) La aplicación también tendrá una parte de estadísticas, para esto nos
-solicitan dar el total de un día en particular, por ejemplo del día 3, acá
-también tengamos en cuenta lo que ocurre con las filas, ya que las columnas
-también comienzan en 0.
-*/
 
 //_________________________DIA_______________________________
 //contador
 let suma_dia = 0; 
 
  //array
-//  let dia = [];  //creo un array para almacenar los elementos de la semana
+
 
 let nroDia = (prompt( "Ingrese el numero del dia: " )); 
 let idDia =nroDia  - 1; //ahora traducimos dia nro a elemento de la matriz
 
 // for para recorrer
 
-     for (let fila=0; fila<matriz[idDia].length; fila++){ 
+     for (let fila=0; fila<matriz.length; fila++){ 
      
-        suma_semana += matriz[fila][idDia]
+        suma_dia += matriz[fila][idDia]
+        suma_dia_particular = matriz[idSemana][idDia]
 
     }
 
-console.log(`el gasto del dia numero ${nroDia} es: ${suma_dia}`);
+//EXTRA. el switch es porque se puede agregar el dia escrito en la terminal.   
+switch (idDia) {
+    case 1 :
+       idDia = " lunes"
+       break;
+    case 2 :
+       idDia = " martes"
+       break;
+    case 3 :
+       idDia = " miercoles"
+       break;
+    case 4 :
+       idDia = " jueves"
+       break;
+    case 5 :
+       idDia = " viernes"
+       break;
+    case 6 :
+       idDia = " sabado"
+       break;
+    case 7 :
+       idDia = " domingo"
+       break;                 
+    default:
+}
+
+console.log(`el gasto de los dias ${idDia} del mes es: ${suma_dia}`);
+console.log(`el gasto del dia ${idDia}  de la semana ${idSemana} es: ${suma_dia_particular}`);
 
 
+//_________________________MES_______________________________
+//contador
+let suma_mes = 0; 
 
+ //array
+  let mes = [];  //creo un array para almacenar los elementos del mes
+
+// for para recorrer primero filas, luego columnas
+
+     for (let fila=0; fila<matriz.length; fila++){ 
+         for (let columna=0; columna<matriz[fila].length;columna++){
+
+        suma_mes += matriz[fila][columna]
+        
+    }}
+console.log(`(F) el gasto del mes son: ${suma_mes} `);
+
+
+// for para recorrer primero filas, luego columnas
+//contador
+let suma_mes2 = 0; 
+
+    for (let columna=0; columna<matriz[0].length;columna++){
+         for (let fila=0; fila<matriz.length; fila++){ 
+         
+        suma_mes2 += matriz[fila][columna]
+        
+    }}
+console.log(`(C) el gasto del mes son: ${suma_mes2} `);
+//Es lo mismo recorer primero por filas o por columnas.
+
+/*d) Obtener cuál fue la semana que más gastos se realizaron. Indicar el día
+que más gastos se realizaron.
+✔ Posibles matrices para comprobar los resultados.
+*/
+
+//_________________________semana con mas gasto____________________
+//contador
+let maxSemana = 0;
+let idSem = 0; 
+
+
+// for para recorrer primero filas, luego columnas
+
+     for (let fila=0; fila<matriz.length; fila++){                      //para cada fila
+        let sumaSem = 0;
+
+        for (let columna=0; columna<matriz[fila].length;columna++){    //recorro columnas
+            sumaSem += matriz[fila][columna]                              //sumo todos los dias de la semana
+        }
+
+        if (sumaSem >maxSemana){
+            maxSemana = sumaSem ;  //reescribo el maximo
+            idSem = fila + 1;   // identifico semana
+        }
+
+    }
+console.log(`la semana con mas gasto fue la semana ${idSem} con: ${maxSemana} `);
+
+//_________________________dia con mas gasto____________________
+//contador
+let maxDia = 0;
+let idDDia = 0; 
+let posicionDia = [0][0];  //elijo el primer elemento de la matriz
+let semana3
+// for para recorrer primero filas, luego columnas
+
+     for (let fila=0; fila<matriz.length; fila++){                      //para cada fila
+        for (let columna=0; columna<matriz[fila].length;columna++){    //recorro columnas
+        
+            if (matriz[fila][columna] >maxDia){    // si el elemento recorrido es mayor que el maximo
+                maxDia = matriz[fila][columna] ;  //reescribo el maximo
+                semana3 = fila + 1;   // identifico semana
+                idDDia = columna + 1;   // identifico el dia
+            }
+        }
+    }
+
+//EXTRA. el switch es porque se puede agregar el dia escrito en la terminal.   
+switch (idDDia) {
+    case 1 :
+       idDDia = " lunes"
+       break;
+    case 2 :
+       idDDia = " martes"
+       break;
+    case 3 :
+       idDDia = " miercoles"
+       break;
+    case 4 :
+       idDDia = " jueves"
+       break;
+    case 5 :
+       idDDia = " viernes"
+       break;
+    case 6 :
+       idDDia = " sabado"
+       break;
+    case 7 :
+       idDDia = " domingo"
+       break;                 
+    default:
+}
+console.log(`el dia con mas gasto fue el dia ${idDDia} de la semana ${semana3} con: ${maxDia}`);
